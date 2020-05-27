cbc5b3f7f40007ffcb46d45f8a41ffae8b86d184
angeliquecorbisier <angeliquecorbisier@gmail.com>
2020-05-27 10:14:27 +0200

    idcontentcategory

--- wp-content/themes/blogshop/template-parts/content/content-single.php
+++ wp-content/themes/blogshop/template-parts/content/content-single.php
@@ -39,4 +39,4 @@
 		</div>
 		<?php endif; ?>
 	</div>
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>#post-<?php the_ID(); ?>
--- wp-content/themes/blogshop/template-parts/content/content.php
+++ wp-content/themes/blogshop/template-parts/content/content.php
@@ -10,15 +10,12 @@ $get_blog_layout = get_theme_mod('blog_layout');
 $getalignment = get_theme_mod( 'article_alignment', 'center' );
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'blogshop-standard-post' ); ?>>
-	<div class="blogshop-standard-post__entry-content text-<?php echo esc_attr( $getalignment );?>">
-		<div class="blogshop-standard-post__categories">
-			<?php blogshop_categories(); ?>
-		</div>
-		<div class="blogshop-standard-post__post-title">
-			
-			<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-		</div>
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'blogshop-standard-post articlegen' ); ?>>
+
+<!--Images-->
+<div class="imagesarticles">
 		<?php
 		if (function_exists('get_field')) :
 		if (get_post_format() === 'gallery') {
@@ -34,20 +31,58 @@ $getalignment = get_theme_mod( 'article_alignment', 'center' );
 			get_template_part( 'template-parts/content/post-header/header', 'standard' );
 		endif;
 		?>
-		<div class="blogshop-standard-post__blog-meta align-<?php echo esc_attr( $getalignment );?>">
-			<?php blogshop_posted_by(false);?>
+</div>
+
+<div class="textarticle">
+
+	<div class="blogshop-standard-post__entry-content text-<?php echo esc_attr( $getalignment );?>">
+		<div class="blogshop-standard-post__categories categoriespage">
+			<?php blogshop_categories(); ?>
 		</div>
-		<div class="blogshop-standard-post__excerpt">
-			<?php
-			$getexerpt = get_theme_mod( 'excerpt_length', 200 );
-			echo esc_html(blogshop_get_excerpt($getexerpt));
-			?>
-		</div>
-		<div class="blogshop-standard-post__readmore">
-			<?php
-			$get_read_more_text = get_theme_mod( 'readmore_text', __( 'Read More', 'blogshop' ) );
-			 ?>
-			<a href="<?php the_permalink(); ?>"><?php echo esc_html($get_read_more_text); ?></a>
+		<div class="blogshop-standard-post__post-title">
+			
+			<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
 		</div>
 	</div>
+
+		
+<div class="blogshop-standard-post__excerpt">
+					<?php
+					$getexerpt = get_theme_mod( 'excerpt_length', 200 );
+					echo esc_html(blogshop_get_excerpt($getexerpt));
+					?>
+				</div>
+
+</div>
+
+
+
+<div class="readmore">
+
+<div class="blogshop-standard-post__blog-meta align-<?php echo esc_attr( $getalignment );?>">
+					<?php blogshop_posted_by(false);?>
+			
+				
+				<div class="blogshop-standard-post__readmore">
+					<?php
+					$get_read_more_text = get_theme_mod( 'readmore_text', __( 'Read More', 'blogshop' ) );
+					?>
+					<a href="<?php the_permalink(); ?>"><?php echo esc_html($get_read_more_text); ?></a>
+				</div>
+			</div>
+
+
+</div>
+
+</article>
+
+
+
+
+
+		
+
+
+
+	
 </article><!-- #post-<?php the_ID(); ?> -->
